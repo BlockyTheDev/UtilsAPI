a6267be6d78f6f61abe9cd1c072e33426a293c7d
DrachenfeuerHD <35228631+DrachenfeuerHD@users.noreply.github.com>
2022-06-24 23:56:54 +0200

    added new version check

--- src/main/java/dev/dontblameme/utilsapi/main/Main.java
+++ src/main/java/dev/dontblameme/utilsapi/main/Main.java
@@ -4,8 +4,14 @@ import dev.dontblameme.utilsapi.events.command.CommandEvent;
 import dev.dontblameme.utilsapi.events.command.CommandUtils;
 import dev.dontblameme.utilsapi.events.event.EventUtils;
 import dev.dontblameme.utilsapi.inventorybuilder.InventoryListener;
+import org.bukkit.Bukkit;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.logging.Level;
 
 public class Main extends JavaPlugin {
 
@@ -13,10 +19,38 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
+        checkVersion();
+
         instance = this;
         getServer().getPluginManager().registerEvents(new InventoryListener(), this);
 
         listenCustomCommands();
+    }
+
+    private void checkVersion() {
+        try {
+            String githubURL = "https://github.com/DrachenfeuerHD/UtilsAPI/releases/latest";
+            String currentVersion = getDescription().getVersion();
+            URLConnection connection = new URL(githubURL).openConnection();
+
+            connection.connect();
+
+            InputStream is = connection.getInputStream();
+            String url = String.valueOf(connection.getURL());
+
+            is.close();
+
+            String versionOnline = url.split("/")[url.split("/").length - 1].replace("v", "");
+
+            if(!currentVersion.equals(versionOnline)) {
+                getLogger().log(Level.WARNING, "There is a new version of UtilsAPI!");
+                getLogger().log(Level.WARNING, "You can download the new version at {0}", githubURL);
+            }
+
+        } catch (Exception e) {
+            getLogger().log(Level.SEVERE, "Could not check for new version! Error: {0}", e.getMessage());
+        }
     }
 
     public static Main getInstance() {
